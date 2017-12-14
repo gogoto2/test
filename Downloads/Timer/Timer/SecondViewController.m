5ba7bc0b62211c4514848d76daf84ac964d07461
金山 <AB056036jinyongshan@ab-insurance.com>
2017-12-14 18:47:20 +0800

    Initial Commit of Library
    Signed-off-by: 金山 <AB056036jinyongshan@ab-insurance.com>

---
+++ Downloads/Timer/Timer/SecondViewController.m
@@ -0,0 +1,76 @@
+//
+//  SecondViewController.m
+//  Timer
+//
+//  Created by jiang  hao on 2016/10/27.
+//  Copyright © 2016年 jiang  hao. All rights reserved.
+//
+
+#import "SecondViewController.h"
+#import "NSTimer+ZBBlockSupport.h"
+static int i = 0;
+@interface SecondViewController()
+//@property (strong,nonatomic) NSTimer *timer;
+@property (weak,nonatomic) NSTimer *timer;
+@property (strong,nonatomic) CADisplayLink *displayLink;
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad{
+    [super viewDidLoad];
+//    NSLog(@" self Retain count is %ld", CFGetRetainCount((__bridge CFTypeRef)self));
+//    //强引用  NSRunloop ---> NSTimer ---> self
+//    self.timer = [NSTimer timerWithTimeInterval:1 target:self selector:@selector(sayHello) userInfo:nil repeats:YES];
+//    //     NSLog(@"timer Retain count is %ld", CFGetRetainCount((__bridge CFTypeRef)self.timer));
+//    NSLog(@"timer Retain count is %ld", CFGetRetainCount((__bridge CFTypeRef)_timer));
+//    NSLog(@" self Retain count is %ld", CFGetRetainCount((__bridge CFTypeRef)self));
+//    //    [[NSRunLoop currentRunLoop] addTimer:self.timer forMode:NSRunLoopCommonModes];
+//    [[NSRunLoop currentRunLoop] addTimer:_timer forMode:NSRunLoopCommonModes];
+//    //     NSLog(@"timer Retain count is %ld", CFGetRetainCount((__bridge CFTypeRef)self.timer));
+//    NSLog(@"timer Retain count is %ld", CFGetRetainCount((__bridge CFTypeRef)_timer));
+    
+    //使用分类的方法,利用block
+    //    __weak typeof(self)  weakSelf = self;
+    //    self.timer = [NSTimer zb_timerWithTimeInterval:1 block:^{
+    //        [weakSelf sayHello];
+    //    }
+    //                                           repeats:YES];
+    //    [[NSRunLoop currentRunLoop] addTimer:self.timer forMode:NSRunLoopCommonModes];
+    
+    //使用iOS10的新方法,看样子iOS10,苹果爸爸给我们封装好了方法,不会再强引用self了
+    //    if ([UIDevice currentDevice].systemVersion.floatValue == 10.0) {
+    //        __weak typeof(self)  weakSelf = self;
+    //        self.timer = [NSTimer timerWithTimeInterval:1.0 repeats:YES block:^(NSTimer * _Nonnull timer) {
+    //            [weakSelf sayHello];
+    //        }];
+    //        [[NSRunLoop currentRunLoop] addTimer:self.timer forMode:NSRunLoopCommonModes];
+    //    }
+    
+}
+
+- (void)sayHello{
+        NSLog(@"hello%d",i++);
+}
+
+- (void)dealloc{
+    NSLog(@"%@%s",NSStringFromClass([self class]),__func__);
+    NSLog(@"%@%s",NSStringFromClass([self class]),__func__);
+    NSLog(@"timer Retain count is %ld", CFGetRetainCount((__bridge CFTypeRef)_timer));
+    NSLog(@" self Retain count is %ld", CFGetRetainCount((__bridge CFTypeRef)self));
+    [self.timer invalidate];
+}
+
+- (IBAction)invalidTimer:(UIButton *)sender {
+    
+    thirdViewController *contact = [[ContactInfo alloc] ;
+    contact.jid = jid;
+    [self.navigationController pushViewController:contact animated:YES];
+    
+    [self.timer invalidate];
+        NSLog(@"timer Retain count is %ld", CFGetRetainCount((__bridge CFTypeRef)_timer));
+}
+
+
+@end
